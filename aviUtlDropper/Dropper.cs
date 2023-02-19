402dc8cd12718c41fc2d1810ef8554f835487c9d
simana_sch <tktossi@live.com>
2023-02-19 11:57:43 +0900

    とりあえずビルド通るようにした

--- aviUtlDropper/Dropper.cs
+++ aviUtlDropper/Dropper.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Linq;
+using System.IO;
+using System.Text;
+using Codeplex.Data;
 
 namespace aviUtlDropper
 {
     public class Dropper
     {
-        public static void Drop()
+        private const string FileMapName = @"GCMZDrops";
+        public static void Drop(
+            IntPtr ownWindowHandle,
+            IEnumerable<string> filePathes,
+            int stepFrameCount = 0,
+            int layer = 0,
+            int timeoutMilliseconds = -1)
         {
             Mutex mutex = null;
             // mutexによる排他制御ができる場合、mutexによるロックを取得する
@@ -20,14 +31,35 @@ namespace aviUtlDropper
             }
             var data = new COPYDATASTRUCT();
             var lparam = IntPtr.Zero;
+            var handle = IntPtr.Zero;
             bool isMutexLocked = false;
+            var dataAddress = IntPtr.Zero;
+            GcmzDropsData gcmzDropsData = null;
             try
             {
                 // ごちゃまぜドロップスのバージョン情報を読取る
                 // var result = ReadAndValidateGcmzInfo(out var gcmzInfo, mutex != null);
+                handle = OpenFileMapping(FILE_MAP_READ, false, FileMapName);
+                dataAddress = MapViewOfFile(handle, FILE_MAP_READ, 0, 0, UIntPtr.Zero);
+                var GcmzDropsData =
+                    (GcmzDropsData.CurrentLayout)Marshal.PtrToStructure(
+                        dataAddress,
+                        typeof(GcmzDropsData.CurrentLayout));
+                gcmzDropsData = new GcmzDropsData(ref GcmzDropsData);
+                if (dataAddress != IntPtr.Zero)
+                {
+                    UnmapViewOfFile(dataAddress);
+                }
+                if (handle != IntPtr.Zero)
+                {
+                    CloseHandle(handle);
+                }
                 // copyDataStructを作る
+                data = CreateCopyDataStruct(layer, stepFrameCount, filePathes);
                 // lparamを作る
                 lparam = Marshal.AllocHGlobal(Marshal.SizeOf(data));
+                // sendMessageの呼び出し
+                // SendMessage();
             }
             catch (Exception ex)
             {
@@ -45,9 +77,47 @@ namespace aviUtlDropper
                     mutex.Dispose();
                 }
             }
-
-
         }
+        /// <summary>
+        /// 『ごちゃまぜドロップス』 v0.3.12 以降用の COPYDATASTRUCT 値を作成する。
+        /// </summary>
+        /// <param name="layer">レイヤー位置指定。</param>
+        /// <param name="frameAdvance">ドロップ後に進めるフレーム数。</param>
+        /// <param name="files">ファイルパス列挙。</param>
+        /// <returns>
+        /// COPYDATASTRUCT 値。
+        /// DataAddress フィールドは利用後に Marshal.FreeHGlobal で解放する必要がある。
+        /// </returns>
+        private static COPYDATASTRUCT CreateCopyDataStruct(
+            int layer,
+            int frameAdvance,
+            IEnumerable<string> files)
+        {
+            // 送信JSON文字列作成
+            var json = DynamicJson.Serialize(new { layer, frameAdvance, files });
+            var data = Encoding.UTF8.GetBytes(json);
+
+            // COPYDATASTRUCT 作成
+            var cds = new COPYDATASTRUCT();
+            try
+            {
+                cds.Param = new UIntPtr(1);
+                cds.size = data.Length;
+                cds.intPtr = Marshal.AllocHGlobal(data.Length);
+                Marshal.Copy(data, 0, cds.intPtr, data.Length);
+            }
+            catch
+            {
+                if (cds.intPtr != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(cds.intPtr);
+                }
+                throw;
+            }
+
+            return cds;
+        }
+
 
         #region Win32 API import
         [StructLayout(LayoutKind.Sequential)]
@@ -57,8 +127,45 @@ namespace aviUtlDropper
             public int size;
             public IntPtr intPtr;
         }
+        // ごちゃまぜドロップスのファイルマッピング読み込み関係
+        private const uint FILE_MAP_READ = 0x0004;
 
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr OpenFileMapping(
+            uint desiredAccess,
+            [MarshalAs(UnmanagedType.Bool)] bool inheritHandle,
+            string name);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr MapViewOfFile(
+            IntPtr fileMapHandle,
+            uint desiredAccess,
+            uint fileOffsetHigh,
+            uint fileOffsetLow,
+            UIntPtr numberOfBytesToMap);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr handle);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool UnmapViewOfFile(IntPtr address);
+
+        // ごちゃまぜドロップスのAPI(sendMessage)関係
         private const uint WM_COPYDATA = 0x004A;
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr SendMessage(
+            IntPtr windowHandle,
+            uint message,
+            IntPtr wparam,
+            IntPtr lparam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern IntPtr SendMessage(
+            IntPtr windowHandle,
+            uint message,
+            IntPtr wparam,
+            string lparam);
+
         #endregion
     }
 }
--- aviUtlDropper/GcmzDropsData.cs
+++ aviUtlDropper/GcmzDropsData.cs
@@ -9,7 +9,7 @@ namespace aviUtlDropper
     class GcmzDropsData
     {
         // ごちゃまぜドロップスv0.3.11以前
-        struct LegacyLayout
+        internal struct LegacyLayout
         {
             public int WindowHandle;
             public int Width;
@@ -21,7 +21,7 @@ namespace aviUtlDropper
             public int ApiVersion;
         }
         // ごちゃまぜドロップスv0.3.12以降での構造体
-        struct CurrentLayout
+        internal struct CurrentLayout
         {
             public int WindowHandle;
             public int Width;
@@ -32,5 +32,72 @@ namespace aviUtlDropper
             public int AudioChannel;
             public int ApiVersion;
         }
+        internal GcmzDropsData(ref CurrentLayout data)
+        {
+            this.WindowHandle = new IntPtr(data.WindowHandle);
+            this.Width = data.Width;
+            this.Height = data.Height;
+            this.FrameRateBase = data.VideoRate;
+            this.FrameRateScale = data.VideoScale;
+            this.FrameRate =
+                (data.VideoScale > 0) ? ((decimal)data.VideoRate / data.VideoScale) : 0;
+            this.AudioSampleRate = data.AudioRate;
+            this.AudioChannelCount = data.AudioChannel;
+            this.ApiVersion = data.ApiVersion;
+        }
+
+        /// <summary>
+        /// WM_COPYDATA メッセージ送信先ウィンドウハンドルを取得する。
+        /// </summary>
+        public IntPtr WindowHandle { get; }
+
+        /// <summary>
+        /// WM_COPYDATA 送信先ウィンドウが開かれているか否かを取得する。
+        /// </summary>
+        public bool IsWindowOpened => this.WindowHandle != IntPtr.Zero;
+
+        /// <summary>
+        /// AviUtl拡張編集プロジェクトの横幅設定値を取得する。
+        /// </summary>
+        public int Width { get; }
+
+        /// <summary>
+        /// AviUtl拡張編集プロジェクトの縦幅設定値を取得する。
+        /// </summary>
+        public int Height { get; }
+
+        /// <summary>
+        /// AviUtl拡張編集プロジェクトのフレームレート基準値を取得する。
+        /// </summary>
+        public int FrameRateBase { get; }
+
+        /// <summary>
+        /// AviUtl拡張編集プロジェクトのフレームレートスケール値を取得する。
+        /// </summary>
+        public int FrameRateScale { get; }
+
+        /// <summary>
+        /// AviUtl拡張編集プロジェクトのフレームレートを取得する。
+        /// </summary>
+        public decimal FrameRate { get; }
+
+        /// <summary>
+        /// AviUtl拡張編集プロジェクトの音声サンプリングレートを取得する。
+        /// </summary>
+        public int AudioSampleRate { get; }
+
+        /// <summary>
+        /// AviUtl拡張編集プロジェクトの音声チャンネル数を取得する。
+        /// </summary>
+        public int AudioChannelCount { get; }
+
+        /// <summary>
+        /// 『ごちゃまぜドロップス』の外部連携APIバージョンを取得する。
+        /// </summary>
+        /// <remarks>
+        /// <see cref="GcmzInfo(ref Data)"/> コンストラクタで作成した場合は 0 を返す。
+        /// </remarks>
+        public int ApiVersion { get; }
+
     }
 }
